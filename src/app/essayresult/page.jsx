ec944dedd194ba285601f082e4a26aca35f6122e
Ruslan0770 <ruslania@code.edu.az>
2024-07-23 14:14:05 -0700

    splitMistakes

--- src/app/essayresult/page.jsx
+++ src/app/essayresult/page.jsx
@@ -9,6 +9,8 @@ import { getCookie } from "cookies-next";
 
 export default function Result() {
   const [highlightedText, setHighlightedText] = useState("");
+  const [finalScore, setFinalScore] = useState(null);
+  const [rightWords, setRightWords] = useState([]);
 
   useEffect(() => {
     const transcription = getCookie("userTranscription");
@@ -17,6 +19,16 @@ export default function Result() {
     if (transcription && errors) {
       const highlighted = highlightErrors(transcription, errors);
       setHighlightedText(highlighted);
+
+      // finalScore'u errors dan gotur
+      const score = errors.find(error => error.finalScore)?.finalScore;
+      if (score !== undefined) {
+        setFinalScore(score);
+      }
+
+      // line1Content değerlerini rightWords'e ekle, split hatalarını tekil hale getir
+      const wordsSet = new Set(errors.map(error => error.line1Content).filter(Boolean));
+      setRightWords(Array.from(wordsSet));
     }
   }, []);
 
@@ -24,7 +36,6 @@ export default function Result() {
     let newText = text;
     let offset = 0;
 
-    
     const allErrors = errors.flatMap(error => {
       if (error.line2ContentIndexes && error.line2ContentIndexes.length > 0) {
         return error.line2ContentIndexes.map(index => ({
@@ -61,6 +72,9 @@ export default function Result() {
             }}
           >
             <h3 className="text-center">İmla</h3>
+            {finalScore !== null && (
+              <p style={{color:"red",fontWeight:"bold"}}  className="text-center">Final Score: {finalScore}</p>
+            )}
           </div>
           <div
             style={{
@@ -70,6 +84,21 @@ export default function Result() {
             }}
             dangerouslySetInnerHTML={{ __html: highlightedText }}
           >
+          </div>
+          <div
+           style={{
+            paddingLeft: "30px",
+            paddingRight: "30px",
+            paddingTop: "30px",
+          }}
+          >
+          <div>
+          <span style={{color:"blue"}}>Right Words:</span>
+              {rightWords.map((word, index) => (
+                <span style={{color:"green"}} key={index}> {word},</span>
+              ))}
+          </div>
+           
           </div>
           <div className={`text-center ${styles.buttonDiv}`}>
             <Link href="/">
@@ -81,4 +110,3 @@ export default function Result() {
     </div>
   );
 }
-
